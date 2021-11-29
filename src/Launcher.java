333e62c45e2f489f5faeea65d209754a70bf0743
benjamin <benjaminh@live.fr>
2021-11-29 16:22:25 +0100

    Map

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,11 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import static java.lang.System.exit;
 
@@ -18,6 +25,9 @@ public class Launcher {
                     scanner.nextLine();
                     System.out.println(Launcher.Fibonacci(nombre));
                 }
+                else if(saisie.equals("freq")){
+                    Launcher.ThreeWords();
+            }
                 else{
                     System.out.println("Unknown command");
                 }
@@ -46,5 +56,28 @@ public class Launcher {
             n1 = resultat;
         }
         return resultat;
+    }
+
+    public static int ThreeWords() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrer le chemin du fichier : ");
+        String path = scanner.nextLine();
+        String text;
+        try {
+            text = java.nio.file.Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            System.out.print("Unreadable file : IOException ");
+            e.printStackTrace();
+            return -1;
+        }
+        text = text.replaceAll("[,!.?:;(){}]", " ");
+        text = text.toLowerCase(Locale.ROOT);
+        String [] stringTab= text.split(" ");
+        for(int i = 0; i < stringTab.length; i++)
+        {
+            System.out.println(stringTab[i]);
+        }
+        Map<String, Long> ListeDeMot = Arrays.stream(stringTab).collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        return 0;
     }
 }
