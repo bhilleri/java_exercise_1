3e410b3b0695a97c4a12c49d36a1cdf12e58d1cd
benjamin <benjaminh@live.fr>
2021-11-29 14:58:59 +0100

    LauncherBoucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,13 +6,14 @@ public class Launcher {
     public static void main(String args[]){
         System.out.println("Bienvenue");
         Scanner scanner = new Scanner(System.in);
-        String saisie = scanner.nextLine();
-        if(saisie.equals("quit"))
-        {
-            exit(0);
-        }
-        else{
-            System.out.println("Unknown command");
-        }
+        String saisie = null;
+        do {
+            if(saisie != null)
+            {
+                System.out.println("Unknown command");
+            }
+            saisie = scanner.nextLine();
+        }while(! saisie.equals("quit"));
+
     }
 }
