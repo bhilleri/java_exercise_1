458c82d6658343facac94ad303f84deb84c1b9fb
benjamin <benjaminh@live.fr>
2021-11-29 15:20:33 +0100

    Fibonacci

--- src/Launcher.java
+++ src/Launcher.java
@@ -8,12 +8,43 @@ public class Launcher {
         Scanner scanner = new Scanner(System.in);
         String saisie = null;
         do {
+
             if(saisie != null)
             {
-                System.out.println("Unknown command");
+                if(saisie.equals("fibo"))
+                {
+                    System.out.print("Entrer la valeur : ");
+                    int nombre = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println(Launcher.Fibonacci(nombre));
+                }
+                else{
+                    System.out.println("Unknown command");
+                }
             }
             saisie = scanner.nextLine();
         }while(! saisie.equals("quit"));
+    }
 
+    public static int Fibonacci(int nombre)
+    {
+        if(nombre == 0)
+        {
+            return 0;
+        }
+        if(nombre == 1)
+        {
+            return 1;
+        }
+        int n1 = 1;
+        int n2 = 0;
+        int resultat = 0;
+        for(int i = 2; i <= nombre; i++)
+        {
+            resultat = n1+n2;
+            n2 = n1;
+            n1 = resultat;
+        }
+        return resultat;
     }
 }
