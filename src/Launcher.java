61f2a8f1138cf226f6d4f8d595fc00423e1b3226
benjamin <benjaminh@live.fr>
2021-11-30 09:40:36 +0100

    partie 6 retrait des commandes inutilisées 2

--- src/Launcher.java
+++ src/Launcher.java
@@ -17,81 +17,38 @@ public class Launcher {
         System.out.println("Bienvenue");
         Scanner scanner = new Scanner(System.in);
         String saisie = null;
-        do {
+        List <Command> CommandList = new ArrayList<Command>();
+        CommandList.add(new Fibo());
+        CommandList.add(new Freq());
+        CommandList.add(new Quit());
 
+        boolean end = false;
+        do {
+            saisie = scanner.nextLine();
+            int selection = -1;
             if(saisie != null)
             {
-                if(saisie.equals("fibo"))
+                for(int i = 0; i < CommandList.size(); i++)
                 {
-                    System.out.print("Entrer la valeur : ");
-                    int nombre = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println(Launcher.Fibonacci(nombre));
+                    if(CommandList.get(i).Name().equals(saisie))
+                    {
+                        selection = i;
+                    }
                 }
-                else if(saisie.equals("freq")){
-                    Launcher.ThreeWords();
-            }
-                else{
+                if(selection == -1)
+                {
                     System.out.println("Unknown command");
+                    end = false;
+                }
+                else{
+                    end = CommandList.get(selection).run(scanner);
                 }
             }
-            saisie = scanner.nextLine();
-        }while(! saisie.equals("quit"));
+            else{
+                end = false;
+            }
+
+        }while(end == false);
     }
 
-    public static int Fibonacci(int nombre)
-    {
-        if(nombre == 0)
-        {
-            return 0;
-        }
-        if(nombre == 1)
-        {
-            return 1;
-        }
-        int n1 = 1;
-        int n2 = 0;
-        int resultat = 0;
-        for(int i = 2; i <= nombre; i++)
-        {
-            resultat = n1+n2;
-            n2 = n1;
-            n1 = resultat;
-        }
-        return resultat;
-    }
-
-    public static int ThreeWords() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrer le chemin du fichier : ");
-        String path = scanner.nextLine();
-        String text;
-        try {
-            text = Files.readString(Paths.get(path));
-        } catch (IOException e) {
-            System.out.print("Unreadable file : IOException ");
-            e.printStackTrace();
-            return -1;
-        }
-
-        // Récupération des mots
-        text = text.replaceAll("[,!.?:;(){}\n\r]", " ");
-        text = text.toLowerCase(Locale.ROOT);
-        String [] stringTab= text.split(" ");
-        Predicate<String> predicate = s -> !(s.isEmpty());
-
-        // Regroupement des mots pour les compter
-        Map<String, Long> WordMap = Arrays.stream(stringTab).filter(predicate).collect(Collectors.groupingBy(p -> p, Collectors.counting()));
-
-        // Trie des mots
-        List<Map.Entry<String, Integer>> WordList = new ArrayList(WordMap.entrySet());
-        WordList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
-        // Séléction des 3 mots les plus réccurrent
-        WordList = WordList.stream().limit(3).collect(Collectors.toList());
-        WordList.forEach((Map.Entry<String, Integer> element)-> {
-            System.out.println(element.getKey() + " : " + element.getValue());
-        });
-        return 0;
-    }
 }
